5b05af33085a111f38b5e66314c68eb5d7093c3e
Kiritome <62535554+KiritoJabami@users.noreply.github.com>
2023-03-22 13:16:46 +0530

    Add project files.

---
+++ WinMain.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#define UNICODE
+#define _UNICODE
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	const wchar_t* pClassName = L"WindowsMyLover";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"Shreyas CS = High IQ",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	// show the damn window
+	ShowWindow(hWnd, SW_SHOW);
+	//while (true);
+	//return 0;
+
+	//message 
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
